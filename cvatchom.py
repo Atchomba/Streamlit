68f49db1181666ccc546cf70d992b1435f768a29
OumaymaDourrou <salamoumayma@gmail.com>
2023-04-14 17:47:47 +0200

    lohugiu

---
+++ cvatchom.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.title('Atout CHOMBA')
+st.subheader("Indirizzo: Via Malatesta 33, 41015 Nonantola(Mo)")
+st.subheader( "E-mail: swatchomba@gmail.com e Tel:+393338250551" )
+
+st.subheader("PRESENTAZIONE")
+st.subheader("ESPERIENZA PROFESSIONALI")
+st.subheader("ISTRUZIONE E FORMAZIONE")
+st.subheader("CONOSCENZE LINGUISTICHE")
+st.subheader("CONOSCENZE INFORMATICHE")
